a2d374dcf620a331be06c7f0ed0633e7fa6b5742
Tim <timoty02@yandex.ru>
2024-10-31 01:25:46 +0300

    checkstyle

--- main_svc/src/main/java/main/server/dao/Category.java
+++ main_svc/src/main/java/main/server/dao/Category.java
@@ -19,6 +19,7 @@ public class Category {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     int id;
     String name;
+
     public Category(String name) {
         this.name = name;
     }
